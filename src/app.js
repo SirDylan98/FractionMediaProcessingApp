0a4ded38562b1eaf0611aff86c58aac9056f91d7
SirDylan98 <dylandzvenetashinga@gmail.com>
2025-03-31 22:43:45 +0200

    Initial commit

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan('dev'));//
 
 // here we are making sure that all folders are present
 const dirs = [
